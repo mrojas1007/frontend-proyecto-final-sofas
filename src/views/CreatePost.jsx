6d24bfebdc32b78b7eba6d4591c18e07cbcfc4c2
Marcela <mrojas634@gmail.com>
2025-02-09 18:03:30 -0300

    modifica createpost

--- src/views/CreatePost.jsx
+++ src/views/CreatePost.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 const CreatePost = () => {
     const [formData, setFormData] = useState({
@@ -45,129 +46,89 @@ const CreatePost = () => {
     return (
         <Container>
             <h1>Formulario de publicación</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formName">
+            <Form onSubmit={handleSubmit} className='col-10 col-md-6 mx-auto p-3 border border-dark rounded my-5'>
+                <Form.Group className="mb-3" controlId="formName">
                     <Form.Label>Nombre</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
+                    <Form.Control type="text" name="name" value={formData.name} onChange={handleChange} required />
                 </Form.Group>
-
-                <Form.Group controlId="formColor">
-                    <Form.Label>Color</Form.Label>
-                    <Form.Control
-                        as="select"
-                        name="color"
-                        value={formData.color}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="">Selecciona</option>
-                        <option value="red">Rojo</option>
-                        <option value="blue">Azul</option>
-                        <option value="green">Verde</option>
-                        <option value="yellow">Amarillo</option>
-                        <option value="black">Negro</option>
-                        <option value="white">Blanco</option>
-                    </Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="formWidth">
-                    <Form.Label>Ancho</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="width"
-                        value={formData.width}
-                        onChange={handleChange}
-                        required
-                    />
-                </Form.Group>
-
-                <Form.Group controlId="formHeight">
-                    <Form.Label>Alto</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="height"
-                        value={formData.height}
-                        onChange={handleChange}
-                        required
-                    />
-                </Form.Group>
-
-                <Form.Group controlId="formBrand">
-                    <Form.Label>Marca</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="brand"
-                        value={formData.brand}
-                        onChange={handleChange}
-                        required
-                    />
-                </Form.Group>
-
-                <Form.Group controlId="formType">
-                    <Form.Label>Tipo</Form.Label>
-                    <Form.Control
-                        as="select"
-                        name="type"
-                        value={formData.type}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="">Selecciona</option>
-                        <option value="sofa">Sofá</option>
-                        <option value="sillon">Sillón</option>
-                    </Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="formBody">
-                    <Form.Label>Cuerpo</Form.Label>
-                    <Form.Control
-                        as="select"
-                        name="body"
-                        value={formData.body}
-                        onChange={handleChange}
-                        required
-                    >
-                        <option value="">Selecciona</option>
-                        <option value="1 cuerpo">1 cuerpo</option>
-                        <option value="2 cuerpos">2 cuerpos</option>
-                        <option value="3 cuerpos">3 cuerpos</option>
-                    </Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="formDescription">
+                <Row>
+                    <Col xs={12} md={4}>
+                        <Form.Group className="mb-3" controlId="formColor">
+                            <Form.Label>Color</Form.Label>
+                            <Form.Control as="select" name="color" value={formData.color} onChange={handleChange} required >
+                                <option value="">Selecciona</option>
+                                <option value="red">Rojo</option>
+                                <option value="blue">Azul</option>
+                                <option value="green">Verde</option>
+                                <option value="yellow">Amarillo</option>
+                                <option value="black">Negro</option>
+                                <option value="white">Blanco</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formWidth">
+                            <Form.Label>Ancho</Form.Label>
+                            <Form.Control type="text" name="width" value={formData.width} onChange={handleChange} required />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formHeight">
+                            <Form.Label>Alto</Form.Label>
+                            <Form.Control type="text" name="height" value={formData.height} onChange={handleChange} required />
+                        </Form.Group></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBrand">
+                            <Form.Label>Marca</Form.Label>
+                            <Form.Control type="text" name="brand" value={formData.brand} onChange={handleChange} required />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formType">
+                            <Form.Label>Tipo</Form.Label>
+                            <Form.Control as="select" name="type" value={formData.type} onChange={handleChange} required >
+                                <option value="">Selecciona</option>
+                                <option value="sofa">Sofá</option>
+                                <option value="sillon">Sillón</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBody">
+                            <Form.Label>Cuerpo</Form.Label>
+                            <Form.Control as="select" name="body" value={formData.body} onChange={handleChange} required >
+                                <option value="">Selecciona</option>
+                                <option value="1 cuerpo">1 cuerpo</option>
+                                <option value="2 cuerpos">2 cuerpos</option>
+                                <option value="3 cuerpos">3 cuerpos</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group className="mb-3" controlId="formDescription">
                     <Form.Label>Descripción</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        name="description"
-                        value={formData.description}
-                        onChange={handleChange}
-                        required
-                    />
+                    <Form.Control as="textarea" name="description" value={formData.description} onChange={handleChange} required />
                 </Form.Group>
-
-                <Form.Group controlId="formPrice">
-                    <Form.Label>Precio</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="price"
-                        value={formData.price}
-                        onChange={handleChange}
-                        required
-                    />
-                </Form.Group>
-
-                <Button variant="secondary" onClick={handleCancel}>
-                    Cancelar
-                </Button>
-                <Button variant="primary" type="submit">
-                    Publicar
-                </Button>
+                <Row>
+                    <Col>
+                        <Form.Group controlId="formPrice">
+                            <Form.Label>Precio</Form.Label>
+                            <Form.Control type="text" name="price" value={formData.price} onChange={handleChange} required />
+                        </Form.Group>
+                    </Col>
+                    <Col className='d-flex align-items-end'>
+                        <Button variant="secondary" onClick={handleCancel} className='me-2'>
+                            Cancelar
+                        </Button>
+                    </Col>
+                    <Col className='d-flex align-items-end'>
+                        <Button variant="success" type="submit">
+                            Publicar
+                        </Button>
+                    </Col>
+                </Row>
             </Form>
         </Container>
     );
