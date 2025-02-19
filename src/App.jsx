98cbaadbcb064efa18fc6b5a4ddc2d40018a133c
Marcela <mrojas634@gmail.com>
2025-02-19 01:54:33 -0300

    fix footer responsive

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
+      <div style={{minHeight: "80vh"}}></div>
       <Footer />
     </UserProvider>
   );
