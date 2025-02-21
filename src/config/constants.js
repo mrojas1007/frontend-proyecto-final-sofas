0270dea81719ece5ab3303ffae71b35e9d2da343
Marcela <mrojas634@gmail.com>
2025-02-20 23:20:11 -0300

    fix: env var

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,7 @@
 // const dotenv = require('dotenv').config();
 
-export const URLBASE = import.meta.env.URL_BACKEND;
+export const URLBASE = import.meta.env.VITE_URL_BACKEND;
+console.log(URLBASE);
 
 export const ENDPOINT = {
   products: `${URLBASE}/producto`,
